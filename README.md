0ecf79274b46169e84923408f0fdf624c4f969b5
Eric Larson <elarson@akamai.com>
2022-07-20 23:23:46 -0400

    Clarify README, cooresponding changes to code

--- README.md
+++ README.md
@@ -4,9 +4,19 @@ Call Scala from Java
 Demonstrate calling from Java code a method written in Scala
 </p>
 This is useful for investigating conversion of argument types to parameter types.  
+</p>
 The problem that inspired this research was that the following Java code create a NullPointerException:
 
 ```
-scalaMethod(1 == 0? 0L: new
+new ScalaClass(0 == 1 ? 66 : 0 == 1 ? new Integer(123) : null);
+```
+
+But the same code, except with 66 passed as an object, did not cause an exception:  
+```
+new ScalaClass(0 == 1 ? new Integer(66) : 0 == 1 ? new Integer(123): null);
 ``` 
+
+The curious part is that neither the primitive nor the object form of 66 would ever be sent to the 
+called method because it is in the "then" clause of an impossible case, so should not have any impact
+on whether the other clauses send in a null.
 
--- src/main/java/javaHello/javaHelloWorld.java
+++ src/main/java/javaHello/javaHelloWorld.java
@@ -6,14 +6,14 @@ public class javaHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         String aString = "abc";
-        Integer anInteger = Integer.getInteger("123");
+        Integer anInteger = new Integer(123);
         new ScalaClass(null);
         new ScalaClass(0 == 1 ? anInteger: null);
         new ScalaClass(0 == 1 ? null : anInteger);
-        new ScalaClass(0 == 1 ? new Integer(0) : 0 == 1 ? anInteger : null);
+        new ScalaClass(0 == 1 ? new Integer(66) : 0 == 1 ? anInteger : null);
         // The following will throw a NullPointerException, but the previous lines won't
         try {
-            new ScalaClass(0 == 1 ? 0 : 0 == 1 ? anInteger : null);
+            new ScalaClass(0 == 1 ? 66 : 0 == 1 ? anInteger : null);
         } catch (NullPointerException npe) {
             System.out.println("Threw null pointer");
         }
